40f464f458db24ee2a3ef3aea5f99ae5e30637f9
Shon M <him@shonm.com>
2013-04-24 19:58:24 -0600

    Tests can just be placeholders for now

--- test/Sonido/SonidoTest.php
+++ test/Sonido/SonidoTest.php
@@ -2,45 +2,12 @@
 
 namespace Sonido;
 
+use Sonido\Adapter\Redis\Queue as RedisQueue;
+
 class SonidoTest extends TestCase
 {
-    public function setUp()
+    public function testEverything()
     {
-        $this->sonido = new Sonido;
-    }
-
-    public function testEnqueueValidClass()
-    {
-        $this->sonido->enqueue('Test_Job', array(), null, false);
-
-        $this->markTestIncomplete();
-    }
-
-    public function testEnqueueInvalidClassThrows()
-    {
-        $this->sonido->enqueue('Invalid_Test_Job', array(), null, false);
-
-        $this->markTestIncomplete();
-    }
-
-    public function testEnqueueWithTracking()
-    {
-        $this->sonido->enqueue('Test_Job', array(), null, true);
-
-        $this->markTestIncomplete();
-    }
-
-    public function testEnqueueWithoutTracking()
-    {
-        $this->sonido->enqueue('Test_Job', array(), null, false);
-
-        $this->markTestIncomplete();
-    }
-
-    public function testEnqueueInfersQueueWhenNull()
-    {
-        $this->sonido->enqueue('Test_Job', array(), null, false);
-
         $this->markTestIncomplete();
     }
 }
